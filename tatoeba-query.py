2353ba7fa5df8bf588969eb0e17ba3a86be12103
eliasrodrigo <eliasrodrigo@protonmail.com>
2017-02-19 00:21:20 +0100

    basic use of argparse in instead of argv

--- tatoeba-query.py
+++ tatoeba-query.py
@@ -1,15 +1,25 @@
-import webbrowser 
-from sys import argv
+import webbrowser, argparse, sys
+#from sys import argv
 
-script, command, flang, tlang, term = argv
+# script, command, flang, tlang, term = argv
 
-fRef = '&from='
-tRef = '&to='
+parser = argparse.ArgumentParser()
 
-search = term + fRef + flang + tRef + tlang
+parser.add_argument("-b", help="Open a browser and show the result", nargs=3)
+
+args = parser.parse_args()
+
+fromLanguage = args.b[0]
+toLanguage = args.b[1]
+term = args.b[2]
+
+fromReference = '&from='
+toReference = '&to='
+
+search = term + fromReference + fromLanguage + toReference + toLanguage
 
 
-if command == 'b':
+if args.b:
     webbrowser.open('https://tatoeba.org/eng/sentences/search?query=' + search, new=2)
 
 else:
