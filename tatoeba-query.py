377da43afd13112cd7401b5a63aa4f9384907548
eliasrodrigo <eliasrodrigo@protonmail.com>
2017-02-25 12:27:37 +0100

    add '-s' to ArgumentParser and break long lines

--- tatoeba-query.py
+++ tatoeba-query.py
@@ -5,8 +5,12 @@ import webbrowser, argparse, sys, os, csv
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-b", help="Open a browser and show the result", nargs=3)
-parser.add_argument("-f", help="Find sentence containing term in a specific language", nargs=2)
-parser.add_argument("-l", help="List languages and their abbreviation used by Tatoeba", nargs=1)
+parser.add_argument("-f", help="Find sentence containing term in a specific \
+        language", nargs=2)
+parser.add_argument("-l", help="List languages and their abbreviation used by \
+        Tatoeba", nargs=1)
+parser.add_argument("-s", help="Search for sentences containing term in a \
+        specific language and it the counterparts of the sentence in another language", nargs=3)
 
 args = parser.parse_args()
 
